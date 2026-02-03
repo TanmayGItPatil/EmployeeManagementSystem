7e817fcb61b64bd796c6247e59125640a4238474
Tanmaypatil2612 <tanmaypatilabc@gmail.com>
2026-02-04 05:25:41 +0530

    changed to another instance for docker

--- frontend/vite-project/src/services/api.js
+++ frontend/vite-project/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://34.233.120.133:8000';
+const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://98.93.245.14:8000';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
